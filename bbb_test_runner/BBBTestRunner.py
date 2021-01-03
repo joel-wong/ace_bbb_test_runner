78626d56f127e7edf4a75eb8b3a2fbbdd40d7606
Joel Wong <joel.wong@ucalgary.ca>
2021-01-02 19:56:54 -0600

    Parse input data, break down requests into components

--- bbb_test_runner/BBBTestRunner.py
+++ bbb_test_runner/BBBTestRunner.py
@@ -1,8 +1,55 @@
+import base64
+import json
+
 
 class BBBTestRunner:
 
     def __init__(self):
         pass
 
-    def run_test(self, test_config):
-        return test_config
+    def run_test(self, test_config_str):
+        """
+        :param test_config_str: str: The test configuration. Must be valid json
+        :return: str: The test results. Valid json
+        """
+        test_config_dict = BBBTestRunner.parse_config(test_config_str)
+        self.handle_i2c(test_config_dict["i2c"])
+        self.handle_outputs(test_config_dict["outputs"])
+
+        test_results_dict = {
+            "inputs": self.get_inputs(test_config_dict["inputs"])
+        }
+
+        return json.dumps(test_results_dict)
+
+    @staticmethod
+    def parse_config(test_config):
+        """
+        Parses a config file to a dictionary
+
+        :param test_config: str: The test configuration. Must be valid json
+        :return: dict: The import json in dictionary form
+        """
+        parsed_test_config = json.loads(test_config)
+        for key in parsed_test_config["i2c"].keys():
+            # change all I2C data to binary from base64 encoding
+            base64_data_str = parsed_test_config["i2c"][key]["data"]
+            parsed_test_config["i2c"][key] = base64.b64decode(base64_data_str)
+        return parsed_test_config
+
+    @staticmethod
+    def handle_i2c(i2c_data_dict):
+        pass
+
+    @staticmethod
+    def handle_outputs(bbb_outputs):
+        pass
+
+    @staticmethod
+    def get_inputs(bbb_inputs):
+        results = {}
+        for pin_number in bbb_inputs.keys():
+            if bbb_inputs[pin_number]["type"] == "digital":
+                # return digital high for now
+                results[pin_number] = "1"
+        return results
