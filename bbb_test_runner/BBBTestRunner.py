516dbbbc07ac94f6501498bf3ca69efcae16634f
Joel Wong <joelw321@gmail.com>
2021-03-08 01:24:49 +0000

    Merged in joel/ordered_test_steps (pull request #9)
    Update ace-bbb-test-runner to handle the new format of test specifications of the ace-test-framework
    
    Approved-by: Wendy Yang
    Approved-by: benjamin.forseth

--- bbb_test_runner/BBBTestRunner.py
+++ bbb_test_runner/BBBTestRunner.py
@@ -1,119 +1,175 @@
 import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.GPIO as GPIO
 import json
-import time  # just for testing
 import subprocess
+
+from ace_bbsm import BBB_IO_CONSTANTS as BIC
 
 
 class BBBTestRunner:
 
     def __init__(self):
-        self.test_results_dict = None
+        # list of test results
+        self.test_results = None
+        # continuously run ADC so that we can retrieve values when needed
         ADC.setup()
+        # track things set to output so that we can set them back to
+        # inputs during the next test
+        self.last_digital_outputs = []
 
     def run_test(self, test_config_str):
         """
-        :param test_config_str: str: The test configuration. Must be valid json
-        :return: str: The test results. Valid json
+        :param test_config_str: str: The test configuration. Must be
+            valid json that represents a list
+        :return: str: The test results. Valid json representing a list
         """
-        print("Running test: {}".format(test_config_str))
-        self.test_results_dict = {"inputs": {}}
-        test_config_dict = BBBTestRunner.parse_config(test_config_str)
-        BBBTestRunner.setup_bbb_inputs(test_config_dict["inputs"])
-        BBBTestRunner.handle_i2c(test_config_dict["i2c"])
-        BBBTestRunner.handle_outputs(test_config_dict["outputs"])
-        time.sleep(30)  # just for testing
-        self.get_inputs(test_config_dict["inputs"])
+        # reset all outputs used by previous test back to inputs
+        for pin_number in self.last_digital_outputs:
+            GPIO.setup(pin_number, GPIO.IN)
+        self.last_digital_outputs = []
 
-        # reset all GPIO channels used by application to inputs
-        GPIO.cleanup()
-        return json.dumps(self.test_results_dict)
+        print("Running test: {}".format(test_config_str))
+
+        self.test_results = []
+        test_specification_list = BBBTestRunner.parse_config(test_config_str)
+        for test_command in test_specification_list:
+            if test_command[BIC.SPEC_TYPE] == BIC.SPEC_TYPE_OUTPUT:
+                self.handle_output(test_command)
+            elif test_command[BIC.SPEC_TYPE] == BIC.SPEC_TYPE_INPUT:
+                self.handle_input(test_command)
+            else:
+                raise ValueError("spec type is neither input nor output")
+
+        return json.dumps(self.test_results)
 
     @staticmethod
     def parse_config(test_config):
         """
-        Parses a config file to a dictionary
+        Parses a config file to a list
 
         :param test_config: str: The test configuration. Must be valid json
-        :return: dict: The import json in dictionary form
+        :return: list: The import json in dictionary form
         """
-        return json.loads(test_config)
+        test_commands = json.loads(test_config)
+        if not type(test_commands) == list:
+            raise ValueError("inputs and outputs must be sent in a list")
+        return test_commands
 
-    @staticmethod
-    def setup_bbb_inputs(bbb_inputs):
-        for pin_number in bbb_inputs.keys():
-            input_type = bbb_inputs[pin_number]["type"]
-            if input_type == "digital":
-                GPIO.setup(pin_number, GPIO.IN)
-            elif input_type == "analog":
-                # adc already started, no action required
-                pass
-            else:
-                raise TypeError(
-                    "The input type {} on pin {} is not supported".format(
-                        input_type, pin_number))
+    def handle_output(self, output_specification):
+        """
+        Outputs the appropriate signal based on the parameters in the
+        output specification
 
-    @staticmethod
-    def handle_i2c(i2c_dict):
-        i2c_spec_keys = sorted(i2c_dict.keys())
-        for key in i2c_spec_keys:
-            i2cbus = i2c_dict[key]["i2cbus"]
-            chip_address = i2c_dict[key]["chip_address"]
-            data_address = i2c_dict[key]["data_address"]
-            subprocess_args = ["i2cset", "-y", i2cbus, chip_address,
-                               data_address]
-            data = i2c_dict[key]["data"]
-            if data != "":
-                subprocess_args.append(data)
-            subprocess.run([" ".join(subprocess_args)], shell=True, check=True)
+        :param output_specification: dict: The output specification
+        """
+        output_type = output_specification[BIC.OUTPUT_TYPE]
+        if output_type == BIC.DIGITAL_3V3:
+            self.handle_digital_output(output_specification)
+        elif output_type == BIC.I2C:
+            BBBTestRunner.handle_i2c(output_specification)
+        else:
+            raise TypeError(
+                "Output type {} is not supported".format(output_type))
 
-    @staticmethod
-    def handle_outputs(bbb_outputs):
-        for pin_number in bbb_outputs.keys():
-            output_type = bbb_outputs[pin_number]["type"]
-            if output_type == "digital":
-                BBBTestRunner.handle_digital_output(
-                    pin_number, bbb_outputs[pin_number]["value"])
-            else:
-                raise TypeError(
-                    "Output type {} on pin {} is not supported".format(
-                        output_type, pin_number))
+    def handle_digital_output(self, output_specification):
+        """
+        Sets a digital output on the BBB to either digital high or low.
+        Required parameters in output_specification:
+        - BIC.PIN_NUMBER
+        - BIC.OUTPUT_VALUE
 
-    @staticmethod
-    def handle_digital_output(pin_number, pin_value):
-        if pin_value == "1":
+        :param output_specification: dict: The parameters for digital output
+        """
+        pin_number = output_specification[BIC.PIN_NUMBER]
+        pin_value = output_specification[BIC.OUTPUT_VALUE]
+        if pin_value == BIC.DIGITAL_HIGH:
             output_value = GPIO.HIGH
-        elif pin_value == "0":
+        elif pin_value == BIC.DIGITAL_LOW:
             output_value = GPIO.LOW
         else:
             raise ValueError(
                 "The input value {} for digital output on pin {} is not "
                 "supported".format(pin_value, pin_number))
         GPIO.setup(pin_number, GPIO.OUT, initial=output_value)
+        self.last_digital_outputs.append(pin_number)
 
-    def get_inputs(self, bbb_inputs):
-        for pin_number in bbb_inputs.keys():
-            input_type = bbb_inputs[pin_number]["type"]
-            if input_type == "digital":
-                self.get_digital_input(pin_number)
-            elif input_type == "analog":
-                self.get_analog_input(pin_number)
-            else:
-                raise TypeError(
-                    "The input type {} on pin {} is not supported".format(
-                        input_type, pin_number))
+    @staticmethod
+    def handle_i2c(i2c_dict):
+        """
+        Performs an i2cset command with the parameters in the i2c_dict.
+        Required keys in the i2c_dict:
+        - BIC.I2CBUS
+        - BIC.I2C_CHIP_ADDRESS
+        - BIC.I2C_DATA_ADDRESS
+        Optional keys in the i2c_dict:
+        - BIC.OUTPUT_VALUE
 
-    def get_digital_input(self, pin_number):
+        :param i2c_dict: dict: The list of parameters for the I2C command
+        """
+        i2cbus = i2c_dict[BIC.I2CBUS]
+        chip_address = i2c_dict[BIC.I2C_CHIP_ADDRESS]
+        data_address = i2c_dict[BIC.I2C_DATA_ADDRESS]
+        subprocess_args = ["i2cset", "-y", i2cbus, chip_address,
+                           data_address]
+        data = i2c_dict[BIC.I2C_DATA]
+        if data != "":
+            subprocess_args.append(data)
+        subprocess.run([" ".join(subprocess_args)], shell=True, check=True)
+
+    def handle_input(self, input_specification):
+        """
+        Inputs a value on a given input pin based on the parameters in
+        the 'input_specification'
+        Will append the input value(s) to 'self.test_results'
+
+        :param input_specification: dict: Parameters for the input
+        """
+        input_type = input_specification[BIC.INPUT_TYPE]
+        if input_type == BIC.DIGITAL_3V3:
+            self.handle_digital_input(input_specification)
+        elif input_type == BIC.ANALOG_1V8:
+            self.handle_analog_input(input_specification)
+        else:
+            raise TypeError(
+                "The input type {} is not supported".format(input_type))
+
+    def handle_digital_input(self, input_specification):
+        """
+        Inputs a digital value on the BIC.PIN_NUMBER entry in the
+        'input_specification' and append the input value to
+        'self.test_results'
+
+        :param input_specification: dict: Parameters for the digital input
+        """
+        pin_number = input_specification[BIC.PIN_NUMBER]
+        GPIO.setup(pin_number, GPIO.IN)
         digital_input_value = GPIO.input(pin_number)
         if digital_input_value == GPIO.HIGH:
-            parsed_input_value = "1"
+            parsed_input_value = BIC.DIGITAL_HIGH
         elif digital_input_value == GPIO.LOW:
-            parsed_input_value = "0"
+            parsed_input_value = BIC.DIGITAL_LOW
         else:
             raise ValueError(
                 "Unexpected digital input {}".format(digital_input_value))
-        self.test_results_dict["inputs"][pin_number] = parsed_input_value
+        self.test_results.append({
+            BIC.INPUT_TYPE: BIC.DIGITAL_3V3,
+            BIC.PIN_NUMBER: pin_number,
+            BIC.INPUT_VALUE: parsed_input_value
+        })
 
-    def get_analog_input(self, pin_number):
+    def handle_analog_input(self, input_specification):
+        """
+        Inputs an analog value on the BIC.PIN_NUMBER entry in the
+        'input_specification' and append the input value to
+        'self.test_results'.
+        The input value will be normalized from 0 to 1
+
+        :param input_specification: dict: Parameters for the analog input
+        """
+        pin_number = input_specification[BIC.PIN_NUMBER]
         analog_input_value = ADC.read(pin_number)
-        self.test_results_dict["inputs"][pin_number] = analog_input_value
+        self.test_results.append({
+            BIC.INPUT_TYPE: BIC.ANALOG_1V8,
+            BIC.PIN_NUMBER: pin_number,
+            BIC.INPUT_VALUE: analog_input_value
+        })
--- bbb_test_runner/TestRunnerManager.py
+++ bbb_test_runner/TestRunnerManager.py
@@ -12,27 +12,65 @@ class TestRunnerManager:
         self.server = ace_bbsm.Server()
 
     def run_server(self):
+        """
+        Run the server. Accept an arbitrary number of clients (one at a
+        time) who then send test specifications which are executed on the
+        BBB
+        """
         print("Starting server...")
         self.server.start_server()
-        while True:
-            try:
-                print("Waiting for incoming connections...")
-                self.server.connect_to_client()
-            except socket.timeout:
-                break
+        try:
             while True:
-                test_config = self.server.receive_from_client()
-                if not test_config:
-                    # Empty string indicates client is ready to close connection
-                    break
-                try:
-                    test_results = self.bbb_test_runner.run_test(test_config)
-                    self.server.send_to_client(test_results)
-                except Exception as e:
-                    print("Error: {}".format(str(e)))
-                    break
-            self.server.disconnect_from_client()
-        self.server.close_server()
+                self.handle_next_client()
+        except KeyboardInterrupt or TimeoutError:
+            # server intentionally stopped or has been inactive for
+            # longer than the timeout specified in the BBSM_CONSTANTS
+            pass
+        finally:
+            self.server.close_server()
+
+    def handle_next_client(self):
+        """
+        Waits for a connection to a client, then executes test
+        specifications sent from that client until the client either
+        intentionally disconnects or there is an error
+        """
+        try:
+            print("Waiting for incoming connections...")
+            print("Press Ctrl+C to manually stop the server")
+            self.server.connect_to_client()
+        except socket.timeout:
+            raise TimeoutError("Socket timeout, closing connection")
+        try:
+            while True:
+                self.service_test_specification()
+        except TestsCompleteInterrupt as e:
+            # tests intentionally finished for a given client
+            print(str(e))
+        except Exception as e:
+            # client has sent invalid data, print error and disconnect
+            print("Error: {}".format(str(e)))
+        self.server.disconnect_from_client()
+
+    def service_test_specification(self):
+        """
+        Receives a test specification from a client, then runs the tests
+        on 'self.bbb_test_runner'
+        """
+        test_config = self.server.receive_from_client()
+        if not test_config:
+            # Empty string indicates client is ready to
+            # close connection
+            raise TestsCompleteInterrupt("Tests finished normally for client")
+        test_results = self.bbb_test_runner.run_test(test_config)
+        self.server.send_to_client(test_results)
+
+
+class TestsCompleteInterrupt(RuntimeError):
+    # An error raised when a client intentionally sends a message
+    # telling the BBB that its tests are completed
+    def __init__(self, message):
+        super().__init__(message)
 
 
 if __name__ == "__main__":
